6cae7a5e111c937bf653aa56a76e843f5cbd7a28
Aleksandr Stepanov <alex_stepanoff@inbox.ru>
2022-04-12 14:06:30 +0300

    made homework

--- Program.cs
+++ Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Diagnostics;
 
 namespace Lesson6_Homework_1
 {
@@ -10,7 +11,24 @@ namespace Lesson6_Homework_1
     {
         static void Main(string[] args)
         {
-            //start homework
+            foreach (var process in Process.GetProcesses())
+            {
+                Console.WriteLine($"ID:|\t{process.Id}\tNAME:|\t{process.ProcessName}");
+                Console.WriteLine("_________________________________________________");
+            }
+
+            string name = string.Empty;
+
+            while (name != "x")
+            {
+                Console.WriteLine("Enter name/id of process to close(Enter \"x\" to exit.)");
+                name = Console.ReadLine();
+
+                if (int.TryParse(name, out int id))
+                    Lesson6.KillById(id);
+                else
+                    Lesson6.KillByName(name);
+            }
         }
     }
 }
